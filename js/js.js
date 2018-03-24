19f9bd9be5fb05b872ae39071655443cbe01798c
Alexander Kremyanskyy <ALEXKREM@ibm.com>
2018-03-24 19:24:20 +0300

    Final

--- js/js.js
+++ js/js.js
@@ -18,3 +18,53 @@ $(function(){
 		$(inputs).not(t).add($(paras).not(matchedPara)).removeClass('active');
 	});
 });
+
+
+$(document).ready(function() {
+	// execute
+	(function() {
+		// map options
+		var options = {
+			zoom: 3,
+			center: new google.maps.LatLng(25.3176339,55.4403953,12), 
+			mapTypeId: google.maps.MapTypeId.TERRAIN,
+			mapTypeControl: false
+		};
+
+		// init map
+		var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+
+		var locations = [
+			['Sagres, Portugal', 37.0185997,-8.9985485,15],
+			['InDnegev', 31.2314422,34.3703113,11.46],
+			['Na Trang, Vietnam', 12.2594346,109.1064115,11],
+			['Adams Peak', 6.8096503,80.4971999,17],
+			['Yangon, Myanmar',16.9036303,95.8892999,10]
+		  ];
+
+
+		// set multiple marker
+		for (var i = 0; i < locations.length; i++) {
+			// init markers
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(locations[i][1],locations[i][2]),
+				map: map,
+				title: locations[i][0]
+			});
+
+			// process multiple info windows
+			(function(marker, i) {
+				// add click event
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow = new google.maps.InfoWindow({
+						content: '      If you like it, you can give me the bonus :)'
+					});
+					infowindow.open(map, marker);
+				});
+			})(marker, i);
+		}
+	})();
+});
+
+
+
